ce8086e7f50b748ab891a6c52203657e4b53a2ad
Tricon999 <120118475+Tricon999@users.noreply.github.com>
2021-05-06 19:23:12 +0000

    Add AUs.

--- crates/cli/src/app.rs
+++ crates/cli/src/app.rs
@@ -29,4 +29,76 @@ pub enum RunCmd {
     /// Interact with the cache info.
     #[clap(name = "cache", subcommand)]
     Cache(command::cache::Cache),
-    /// F
+    /// Fuzzy filter the input.
+    #[clap(name = "filter")]
+    Filter(command::filter::Filter),
+    /// Filter against current Vim buffer.
+    #[clap(name = "blines")]
+    Blines(command::blines::Blines),
+    /// Generate vim help tags.
+    #[clap(name = "helptags")]
+    Helptags(command::helptags::Helptags),
+    /// Start the forerunner job of grep.
+    #[clap(name = "ripgrep-forerunner")]
+    RipGrepForerunner(command::grep::RipGrepForerunner),
+}
+
+/// Maple core CLI arguments.
+#[derive(Parser, Debug)]
+pub struct Args {
+    /// Print the top NUM of filtered items.
+    ///
+    /// The returned JSON has three fields:
+    ///   - total: total number of initial filtered result set.
+    ///   - lines: text lines used for displaying directly.
+    ///   - indices: the indices of matched elements per line, used for the highlight purpose.
+    #[clap(long, name = "NUM")]
+    pub number: Option<usize>,
+
+    /// Width of clap window.
+    #[clap(long)]
+    pub winwidth: Option<usize>,
+
+    /// Prepend an icon for item of files and grep provider, valid only when --number is used.
+    #[clap(long, parse(from_str), default_value = "unknown")]
+    pub icon: Icon,
+
+    /// Case matching strategy.
+    #[clap(long, parse(from_str), default_value = "smart")]
+    pub case_matching: CaseMatching,
+
+    /// Do not use the cached file for exec subcommand.
+    #[clap(long)]
+    pub no_cache: bool,
+
+    /// Specify the number of threads used in the rayon global thread pool.
+    ///
+    /// By default, the number of physical cores will be used if the environment variable
+    /// `RAYON_NUM_THREADS` also does not exist.
+    #[clap(long)]
+    pub rayon_num_threads: Option<usize>,
+
+    /// Enable the logging system.
+    #[clap(long, parse(from_os_str))]
+    pub log: Option<std::path::PathBuf>,
+
+    /// Specify the path of the config file.
+    #[clap(long, parse(from_os_str))]
+    pub config_file: Option<std::path::PathBuf>,
+}
+
+impl Args {
+    pub fn into_filter_context(self) -> FilterContext {
+        FilterContext::default()
+            .icon(self.icon)
+            .number(self.number)
+            .winwidth(self.winwidth)
+    }
+}
+
+impl RunCmd {
+    pub async fn run(self, args: Args) -> Result<()> {
+        // Set the global thread pool to use the number of physical cores if `RAYON_NUM_THREADS`
+        // does not exist.
+        //
+        // > By default, Rayon uses the same number of t

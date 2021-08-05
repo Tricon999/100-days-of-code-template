8eaf17b6b5f774e62e2602f7e65e1855e281f2d7
Tricon999 <120118475+Tricon999@users.noreply.github.com>
2021-08-05 12:05:28 +0000

    Update projects list.

--- crates/maple_core/src/searcher/files.rs
+++ crates/maple_core/src/searcher/files.rs
@@ -46,4 +46,92 @@ fn search_files(
                 _ => return WalkState::Continue,
             };
 
-            let path = if let Ok(p) = entry.path().s
+            let path = if let Ok(p) = entry.path().strip_prefix(&search_root) {
+                p.to_string_lossy().to_string()
+            } else {
+                entry.path().to_string_lossy().to_string()
+            };
+
+            // TODO: Add match_file_path() in matcher to avoid allocation each time.
+            let maybe_matched_item = matcher.match_item(Arc::new(path));
+
+            if let Err(err) = sender.send(maybe_matched_item) {
+                tracing::debug!("Sender is dropped: {err:?}");
+                WalkState::Quit
+            } else {
+                WalkState::Continue
+            }
+        })
+    });
+}
+
+pub async fn search(query: String, hidden: bool, matcher: Matcher, search_context: SearchContext) {
+    let SearchContext {
+        paths,
+        vim,
+        icon,
+        winwidth,
+        stop_signal,
+        item_pool_size,
+    } = search_context;
+
+    let number = item_pool_size;
+    let progressor = VimProgressor::new(vim, stop_signal.clone());
+
+    let (sender, mut receiver) = unbounded_channel();
+
+    std::thread::Builder::new()
+        .name("files-worker".into())
+        .spawn({
+            let stop_signal = stop_signal.clone();
+            move || search_files(paths, hidden, matcher, stop_signal, sender)
+        })
+        .expect("Failed to spawn blines worker thread");
+
+    let mut total_matched = 0usize;
+    let mut total_processed = 0usize;
+
+    let mut best_items = BestItems::new(
+        icon,
+        winwidth,
+        number,
+        progressor,
+        Duration::from_millis(200),
+    );
+
+    let now = std::time::Instant::now();
+
+    while let Some(maybe_matched_item) = receiver.recv().await {
+        if stop_signal.load(Ordering::SeqCst) {
+            return;
+        }
+
+        match maybe_matched_item {
+            Some(matched_item) => {
+                total_matched += 1;
+                total_processed += 1;
+
+                best_items.on_new_match(matched_item, total_matched, total_processed);
+            }
+            None => {
+                total_processed += 1;
+            }
+        }
+    }
+
+    let elapsed = now.elapsed().as_millis();
+
+    let BestItems {
+        items,
+        progressor,
+        winwidth,
+        ..
+    } = best_items;
+
+    let display_lines = printer::to_display_lines(items, winwidth, icon);
+
+    progressor.on_finished(display_lines, total_matched, total_processed);
+
+    tracing::debug!(
+        ?query,
+    

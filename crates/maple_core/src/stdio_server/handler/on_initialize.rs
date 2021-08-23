a9d83043bb4e529a23eeb5e670fb059dd4227cee
Tricon999 <120118475+Tricon999@users.noreply.github.com>
2021-08-23 08:57:31 +0000

    Coq 8.5.dev, repo stable, mode clean.

--- crates/maple_core/src/stdio_server/handler/on_initialize.rs
+++ crates/maple_core/src/stdio_server/handler/on_initialize.rs
@@ -45,4 +45,56 @@ async fn initialize_provider_source(ctx: &Context) -> Result<ProviderSource> {
 
     // Known providers.
     match ctx.provider_id() {
-        "bli
+        "blines" => {
+            let total = count_lines(std::fs::File::open(&ctx.env.start_buffer_path)?)?;
+            let path = ctx.env.start_buffer_path.clone();
+            return Ok(ProviderSource::File { total, path });
+        }
+        "tags" => {
+            let items = crate::tools::ctags::buffer_tag_items(&ctx.env.start_buffer_path, false)?;
+            let total = items.len();
+            return Ok(ProviderSource::Small { total, items });
+        }
+        "proj_tags" => {
+            let ctags_cmd = ProjectCtagsCommand::with_cwd(ctx.cwd.to_path_buf());
+            let provider_source = if ctx.env.no_cache {
+                let lines = ctags_cmd.execute_and_write_cache().await?;
+                to_small_provider_source(lines)
+            } else {
+                match ctags_cmd.ctags_cache() {
+                    Some((total, path)) => ProviderSource::CachedFile {
+                        total,
+                        path,
+                        refreshed: false,
+                    },
+                    None => {
+                        let lines = ctags_cmd.execute_and_write_cache().await?;
+                        to_small_provider_source(lines)
+                    }
+                }
+            };
+            return Ok(provider_source);
+        }
+        "help_tags" => {
+            let helplang: String = ctx.vim.eval("&helplang").await?;
+            let runtimepath: String = ctx.vim.eval("&runtimepath").await?;
+            let doc_tags = std::iter::once("/doc/tags".to_string()).chain(
+                helplang
+                    .split(',')
+                    .filter(|&lang| lang != "en")
+                    .map(|lang| format!("/doc/tags-{lang}")),
+            );
+            let lines = crate::helptags::generate_tag_lines(doc_tags, &runtimepath);
+            return Ok(to_small_provider_source(lines));
+        }
+        _ => {}
+    }
+
+    let source_cmd: Vec<Value> = ctx.vim.bare_call("provider_source").await?;
+    if let Some(value) = source_cmd.into_iter().next() {
+        match value {
+            // Source is a String: g:__t_string, g:__t_func_string
+            Value::String(command) => {
+                // Always try recreating the source.
+                if ctx.provider_id() == "files" {
+                    let mut tokio_cmd = crate::process::tokio::TokioCommand::new

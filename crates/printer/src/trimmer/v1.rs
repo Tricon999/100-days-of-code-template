729a3102239ca5a59f28303b0d565916c7cb6add
Tricon999 <120118475+Tricon999@users.noreply.github.com>
2022-03-03 09:41:39 +0000

    README tweaks

--- crates/printer/src/trimmer/v1.rs
+++ crates/printer/src/trimmer/v1.rs
@@ -130,4 +130,86 @@ pub fn trim_text(
     let w1 = if match_start == 0 {
         0
     } else {
-        acc_widt
+        acc_width[match_start - 1]
+    };
+
+    let w2 = if match_end >= acc_width.len() {
+        full_width - w1
+    } else {
+        acc_width[match_end] - w1
+    };
+
+    let w3 = full_width - w1 - w2;
+
+    if (w1 > w3 && w2 + w3 <= container_width) || (w3 <= 2) {
+        // right-fixed, ..ring
+        let (trimmed_text, trimmed_len) = trim_left(text, container_width - 2, tabstop);
+
+        let text = format!("..{trimmed_text}");
+        let indices = indices
+            .iter()
+            .filter_map(|x| (x + 2).checked_sub(trimmed_len))
+            .filter(|x| *x > 1) // Ignore the highlights in `..`
+            .collect();
+
+        Some((text, indices))
+    } else if w1 <= w3 && w1 + w2 <= container_width {
+        // left-fixed, Stri..
+        let trimmed_text = trim_right(text, container_width - 2, tabstop);
+
+        let text = format!("{trimmed_text}..");
+        let indices = indices
+            .iter()
+            .filter(|x| *x + 2 < container_width) // Ignore the highlights in `..`
+            .copied()
+            .collect::<Vec<_>>();
+
+        Some((text, indices))
+    } else {
+        // Convert the char-position to byte-position.
+        let match_start_byte_idx = text.char_indices().nth(match_start)?.0;
+
+        // left-right, ..Stri..
+        let left_truncated_text = &text[match_start_byte_idx..];
+        let trimmed_text = trim_right(left_truncated_text, container_width - 2 - 2, tabstop);
+
+        let text = format!("..{trimmed_text}..");
+        let indices = indices
+            .iter()
+            .map(|x| x - match_start + 2)
+            .filter(|x| *x + 2 < container_width) // Ignore the highlights in `..`
+            .collect::<Vec<_>>();
+
+        Some((text, indices))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::filter_single_line;
+    use types::MatchedItem;
+
+    #[test]
+    fn test_trim_left() {
+        let text = "0123456789abcdef";
+        let width = 5;
+        let (trimmed, offset) = trim_left(text, width, 4);
+        assert_eq!(trimmed, "bcdef");
+        assert_eq!(offset, 11);
+    }
+
+    #[test]
+    fn test_trim_right() {
+        let text = "0123456789abcdef";
+        let width = 5;
+        let trimmed = trim_right(text, width, 4);
+        assert_eq!(trimmed, "01234");
+    }
+
+    #[test]
+    fn test_trim_text() {
+        // raw_line, query, highlighted, container_width, display_line
+        let test_cases = vec![
+            (
+                "directories/are/nested/a/lot/then/the/

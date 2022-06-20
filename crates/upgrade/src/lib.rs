3e3cb1a65c3eefe70f2340d150df922149ba74b8
Tricon999 <120118475+Tricon999@users.noreply.github.com>
2022-06-20 07:28:55 +0000

    hourly server push

--- crates/upgrade/src/lib.rs
+++ crates/upgrade/src/lib.rs
@@ -126,3 +126,97 @@ fn set_executable_permission<P: AsRef<std::path::Path>>(path: P) -> std::io::Res
 async fn download_prebuilt_binary(
     version: &str,
     no_progress_bar: bool,
+) -> std::io::Result<PathBuf> {
+    let binary_unavailable = || {
+        std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "No available prebuilt binary for this platform",
+        )
+    };
+
+    let asset_name = asset_name().ok_or_else(binary_unavailable)?;
+
+    let mut tmp = std::env::temp_dir();
+    tmp.push(format!("{version}-{asset_name}"));
+
+    let total_size = retrieve_asset_size(asset_name, version).await?;
+
+    // Check if there is a partially downloaded binary before.
+    if tmp.is_file() {
+        let metadata = std::fs::metadata(&tmp)?;
+        if metadata.len() == total_size {
+            println!("{} has alreay been downloaded", tmp.display());
+            return Ok(tmp);
+        } else {
+            std::fs::remove_file(&tmp)?;
+        }
+    }
+
+    let mut maybe_progress_bar = if no_progress_bar {
+        None
+    } else {
+        let progress_bar = ProgressBar::new(total_size);
+        progress_bar.set_style(ProgressStyle::default_bar()
+                       .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+                       .progress_chars("#>-"));
+        Some(progress_bar)
+    };
+
+    let to_io_error =
+        |e| std::io::Error::new(std::io::ErrorKind::Other, format!("Reqwest error: {e}"));
+
+    let download_url = asset_download_url(version).ok_or_else(binary_unavailable)?;
+    let mut source = reqwest::Client::new()
+        .get(download_url)
+        .send()
+        .await
+        .map_err(to_io_error)?;
+
+    let mut dest = tokio::fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&tmp.as_path())
+        .await?;
+
+    while let Some(chunk) = source.chunk().await.map_err(to_io_error)? {
+        dest.write_all(&chunk).await?;
+
+        if let Some(ref mut progress_bar) = maybe_progress_bar {
+            progress_bar.inc(chunk.len() as u64);
+        }
+    }
+
+    #[cfg(unix)]
+    set_executable_permission(&tmp)?;
+
+    println!("Download of '{}' has been completed.", tmp.display());
+
+    Ok(tmp)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_version_number() {
+        let tag = "v0.13-4-g58738c0";
+        assert_eq!(13u32, extract_local_version_number(tag));
+        let tag = "v0.13";
+        assert_eq!(13u32, extract_local_version_number(tag));
+    }
+
+    #[tokio::test]
+    async fn test_download_prebuilt_binary() {
+        for _i in 0..20 {
+            if let Ok(latest_tag) = retrieve_latest_release().await.map(|r| r.tag_name) {
+                download_prebuilt_binary(&latest_tag, true)
+                    .await
+                    .expect("Failed to download the prebuilt binary into a tempfile");
+                return;
+            }
+            tokio::time::sleep(std::time::Duration::from_millis(300)).await;
+        }
+        panic!("Failed to download the prebuilt binary of latest release");
+    }
+}

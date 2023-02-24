15ba01512fa588bfa3f2212a7822dd89ec19e105
Tricon999 <120118475+Tricon999@users.noreply.github.com>
2023-02-24 09:46:14 +0000

    correct formatting

--- pythonx/clap/scorer.py
+++ pythonx/clap/scorer.py
@@ -57,4 +57,101 @@ def subsequence(niddle, haystack):
     """
     Check if niddle is subsequence of haystack
     """
-    niddle, haystack = niddle.low
+    niddle, haystack = niddle.lower(), haystack.lower()
+    if not niddle:
+        return True
+    offset = 0
+    for char in niddle:
+        offset = haystack.find(char, offset) + 1
+        if offset <= 0:
+            return False
+    return True
+
+
+def compute(niddle, haystack):
+    """
+    Calculate score, and positions of haystack
+    """
+    n, m = len(niddle), len(haystack)
+    bonus_score = bonus(haystack)
+
+    if niddle.islower():
+        haystack = haystack.lower()
+
+    if n == 0 or n == m:
+        return SCORE_MAX, list(range(n))
+
+    D = [[0] * m for _ in range(n)]  # best score ending with `niddle[:i]`
+    M = [[0] * m for _ in range(n)]  # best score for `niddle[:i]`
+
+    for i in range(n):
+        prev_score = SCORE_MIN
+        gap_score = SCORE_GAP_TRAILING if i == n - 1 else SCORE_GAP_INNER
+
+        for j in range(m):
+            if niddle[i] == haystack[j]:
+                score = SCORE_MIN
+                if i == 0:
+                    score = j * SCORE_GAP_LEADING + bonus_score[j]
+                elif j != 0:
+                    score = max(
+                        M[i - 1][j - 1] + bonus_score[j],
+                        D[i - 1][j - 1] + SCORE_MATCH_CONSECUTIVE,
+                    )
+                D[i][j] = score
+                M[i][j] = prev_score = max(score, prev_score + gap_score)
+            else:
+                D[i][j] = SCORE_MIN
+                M[i][j] = prev_score = prev_score + gap_score
+
+    return D, M
+
+
+def positions(niddle, haystack):
+    n, m = len(niddle), len(haystack)
+
+    positions = [0] * n
+
+    if n == 0 or m == 0:
+        return positions
+
+    if n == m:
+        return positions
+
+    if m > 1024:
+        return positions
+
+    match_required = False
+
+    D, M = compute(niddle, haystack)
+
+    i, j = n - 1, m - 1
+
+    while i >= 0:
+        while j >= 0:
+            if (match_required or D[i][j] == M[i][j]) and D[i][j] != SCORE_MIN:
+                match_required = (i > 0 and j > 0 and M[i][j]
+                                  == D[i - 1][j - 1] + SCORE_MATCH_CONSECUTIVE)
+                positions[i] = j
+                j -= 1
+                break
+            else:
+                j -= 1
+        i -= 1
+
+    return M[n - 1][m - 1], positions
+
+
+def score(niddle, haystack):
+    """
+    Calculate score, and positions of haystack
+    """
+    n, m = len(niddle), len(haystack)
+    bonus_score = bonus(haystack)
+
+    if niddle.islower():
+        haystack = haystack.lower()
+
+    if n == 0 or n == m:
+        return SCORE_MAX, list(range(n))
+    D = [[0
